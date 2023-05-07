93f4c39d328e21971c366712c72b49887adfc73b
Gaitinov <muxarams@gmail.com>
2023-05-07 22:15:44 +0600

    Ticket data schema updated

--- server/models/Ticket.js
+++ server/models/Ticket.js
@@ -1,10 +1,17 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model } = require('mongoose');
 
 const TicketSchema = new Schema({
   title: String,
   author: String,
   date: Date,
   description: String,
+  messages: [
+    {
+      sender: String,
+      content: String,
+      date: Date,
+    },
+  ],
 });
 
 module.exports = model('Ticket', TicketSchema);
